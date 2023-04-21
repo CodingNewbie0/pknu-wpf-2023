18a1b2a90ca81975da8ebcc98ad8419f3250cfbc
CodingNewbie0 <ahrgjdy@gmail.com>
2023-04-21 13:02:54 +0900

    4월 21일

---
+++ Day04/Day04WpfApp/wp08_personalInfoApp/Models/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wp08_personalInfoApp.Models
+{
+    internal class Person
+    {
+        // 외부에서 접근불가
+        private string email;
+        private DateTime date;
+
+        public string FirstName { get; set; } // Auto Property
+        public string LastName { get; set; }
+        public string Email { get => email; set => email = value; }
+        public DateTime Date { get => date; set => date = value; }
+
+        public Person(string firstName, string lastName, string email, DateTime date)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Date = date;
+        }
+    }
+}
