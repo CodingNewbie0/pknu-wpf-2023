c730209837f7c64a60d96b7dafa27bddbaf85a88
CodingNewbie0 <ahrgjdy@gmail.com>
2023-04-24 17:35:57 +0900

    4월 24일

---
+++ Day05/Day05WpfApp/wp10_employeesApp/Models/Employees.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wp10_employeesApp.Models
+{
+    public class Employees
+    {
+        public int Idx { get; set; }
+        public string Name { get; set; }
+        public int Salary { 
+            get => Salary;
+            set
+            {
+                {
+                    throw new Exception("급여 오류");
+                }
+                else
+                {
+                    Salary = value;
+                }
+            } 
+        }
+        public string DeptName { get; set; }
+        public string Address { get; set; }
+        public string FullName { get; internal set; }
+    }
+}
--- README.md
+++ README.md
@@ -18,11 +18,20 @@ Windows Presentation Foundation
 	
 ## 3일차
 - WPF 기초
-	- 데이터바인딩 계속
+	- 데이터바인딩 마무리
 	- 디자인(템플릿, 스타일, 리소스, 리소스딕셔너리)
 	
 ## 4일차
 - WPF 응용
 	- MahApps.Metro 디자인 적용
 	- MVVM 패턴
+	
+4일차 MVVM View
+
+<img src="https://raw.githubusercontent.com/hugoMGSung/pknu-wpf-2023/main/images/wpf01.png" width="700" />
+	
+## 5일차
+- WPF 응용
+	- MVVM 패턴
+	- MVVM framework/Caliburn
 	- 데이터바인딩 DB연동
